84bba5dabfb0d0243f4378457af5ad6a8bafd68a
acunap <hello@antonioacuna.dev>
2022-12-04 17:11:38 +0100

    Day 4 - Refactor

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,35 +1,12 @@
 fun main() {
-    fun <T : Comparable<T>> ClosedRange<T>.contains(other: ClosedRange<T>): Boolean {
-        return this.start <= other.start && this.endInclusive >= other.endInclusive
+    fun parsePairs(input: List<String>) = input.map { line ->
+        line.split(",").map {
+            it.split("-").map(String::toInt).let { (a, b) -> (a..b).toSet() } }
     }
 
-    fun <T : Comparable<T>> ClosedRange<T>.overlap(other: ClosedRange<T>): Boolean {
-        return this.start >= other.start && this.start <= other.endInclusive || this.endInclusive >= other.start && this.endInclusive <= other.endInclusive
-    }
+    fun part1(input: List<String>) = parsePairs(input).count { (a, b) -> a.containsAll(b) || b.containsAll(a) }
 
-    fun part1(input: List<String>): Int = input.map { line ->
-            line.split(",").map { pair ->
-                pair.split("-")
-            }
-        }.map { pair ->
-            pair.map {
-                it[0].toInt().rangeTo(it[1].toInt())
-            }
-        }.filter { ranges ->
-            ranges[0].contains(ranges[1]) || ranges[1].contains(ranges[0])
-        }.size
-
-    fun part2(input: List<String>): Int = input.map { line ->
-        line.split(",").map { pair ->
-            pair.split("-")
-        }
-    }.map { pair ->
-        pair.map {
-            it[0].toInt().rangeTo(it[1].toInt())
-        }
-    }.filter { ranges ->
-        ranges[0].overlap(ranges[1]) || ranges[1].overlap(ranges[0])
-    }.size
+    fun part2(input: List<String>) = parsePairs(input).count { (a, b) -> a.intersect(b).isNotEmpty() }
 
     val input = readLines("Day04")
     println(part1(input))
@@ -39,13 +16,3 @@ fun main() {
     check(part1(testInput) == 2)
     check(part2(testInput) == 4)
 }
-
-/*
-
-
-.2345678.  2-8
-..34567..  3-7
-
-.....6...  6-6
-...456...  4-6
-*/
